faa7cbd38eabd0ddb98a00bad20628fa32433182
MrMrRobat <appkiller16@gmail.com>
2020-11-01 23:20:57 +0300

    use assert snapshot.match(value) syntax

--- perseus/_vendor/snapshottest/module.py
+++ perseus/_vendor/snapshottest/module.py
@@ -234,7 +234,15 @@ class SnapshotTest(object):
         __tracebackhide__ = True
         assert value == snapshot
 
-    def assert_match(self, value, name=''):
+    def match(self, value, name=''):
+        """
+        Compares given value with stored snapshot
+
+        Returns True or raises AssertionError if value doesn't match current snapshot
+
+        Preferred usage is:
+            assert snapshot.match('foo')
+        """
         self.curr_snapshot = name or self.snapshot_counter
         self.visit()
         if self.update:
@@ -253,6 +261,8 @@ class SnapshotTest(object):
 
         if not name:
             self.snapshot_counter += 1
+
+        return True
 
     def save_changes(self):
         self.module.save()
